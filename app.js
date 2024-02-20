1d4aeee8b7a5bb84b76dd07381ba9b971d26624a
Ad3Ni1 <danielvincentdelacruz99@gmail.com>
2024-02-20 10:51:48 +0800

    item array

--- app.js
+++ app.js
@@ -5,3 +5,49 @@ let listCard = document.querySelector('.listCard');
 let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active')
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active')
+})
+
+let products = [
+    {
+        id: 1,
+        name:'PRODUCT NAME 1',
+        image: '1.PNG',
+        price: 4999
+    },
+    {
+        id: 2,
+        name:'PRODUCT NAME 2',
+        image: '2.PNG',
+        price: 5499
+    },
+    {
+        id: 3,
+        name:'PRODUCT NAME 3',
+        image: '3.PNG',
+        price: 4599
+    },
+    {
+        id: 4,
+        name:'PRODUCT NAME 4',
+        image: '4.PNG',
+        price: 999
+    },
+    {
+        id: 5,
+        name:'PRODUCT NAME 5',
+        image: '5.PNG',
+        price: 1999
+    },
+    {
+        id: 6,
+        name:'PRODUCT NAME 6',
+        image: '6.PNG',
+        price: 4999
+    }
+];
